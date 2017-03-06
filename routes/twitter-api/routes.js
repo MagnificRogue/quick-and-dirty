b7414fe022a38a1977c4e1f22b831d3c6783cb06
longshuicy <chenwang.carrie@gmail.com>
2017-03-06 13:11:49 -0600

    add twitter/account/.. endpoint

--- routes/twitter-api/routes.js
+++ routes/twitter-api/routes.js
@@ -5,11 +5,21 @@ var config = require('../../config');
 var DB = require('../../bin/connectDB');
 var url = require('url');
 var Twitter = require('twitter');
+var client = new Twitter({
+			consumer_key:config.twitter.consumer_key,
+			consumer_secret:config.twitter.consumer_secret,
+			access_token_key:config.twitter.access_token_key,
+			access_token_secret:config.twitter.access_token_secret
+		})
 
 /* home page. */
 router.get('/', function(req, res, next) {
   res.render('twitter-api/index', { title: 'Twitter API Viewer' ,
-                                    endpoints: ['twitter/search', 'twitter/user_timeline','twitter/result']
+                                    endpoints: ['twitter/account/settings',
+												'twitter/account/verify_credentials',
+												'twitter/search', 
+												'twitter/user_timeline',
+												'twitter/result']
                                     });
 });
 
@@ -23,20 +33,45 @@ router.get('/search', function(req, res, next){
     res.send(JSON.stringify({error: 'Parameter $query is required'}));
     return;
   }
-  var client = new Twitter({
-			consumer_key:config.twitter.consumer_key,
-			consumer_secret:config.twitter.consumer_secret,
-			access_token_key:config.twitter.access_token_key,
-			access_token_secret:config.twitter.access_token_secret
-		});
   var response = client.get('search/tweets',{'q':query.query},(error,tweets)=>{
 			res.setHeader('Content-Type','application/json');
 			res.send(JSON.stringify(tweets.statuses));			
 			});
 });
 
+/* account/settings */
+router.get('/account/settings',function(req,res,next){
+	client.get('account/settings',{}, function(error,tweets,response){
+			if(error) throw JSON.stringify(error);
+			res.setHeader('Content-Type','application/json');
+			res.send(JSON.stringify(tweets));
+			//mongoDB
+		});
+});
 
-/* user_timeline */
+/* account/verify_credential */
+router.get('/account/verify_credentials',function(req,res,next){
+	var requestParams = require('./params/credentialsParams');
+	res.render('twitter-api/paramForm',{title:'testing if authentication of the requesting user is successful or not',
+										url: '/twitter/account/verify_credentials',
+										params: requestParams});
+});
+router.post('/account/verify_credentials',function(req,res,next){
+	for (var key in req.body){
+		if (req.body[key] === 'on'){
+			req.body[key] = 'true';
+		}
+	}
+	console.log(req.body);
+	client.get('account/verify_credentials',req.body, function(error,tweets,response){
+		if(error) throw JSON.stringify(error);
+		res.setHeader('Content-Type','application/json');
+		res.send(JSON.stringify(tweets));
+		//mongoDB
+	});
+});
+
+/* user_timeline 
 router.get('/user_timeline',function(req, res, next){
   var requestParams = require('./userTimelineParams'); 
   res.render('twitter-api/userTimeline', {title: 'Render User Timeline', params: requestParams});
@@ -48,22 +83,15 @@ router.post('/user_timeline', function(req, res, next){
 });
 
 var twtTimeline = function(req){
-	var Twitter = require('twitter');
-		var client = new Twitter({
-			consumer_key:config.twitter.consumer_key,
-			consumer_secret:config.twitter.consumer_secret,
-			access_token_key:config.twitter.access_token_key,
-			access_token_secret:config.twitter.access_token_secret
-		})
 		client.get('statuses/user_timeline',req.body, function(error,tweets,response){
 			if(error) throw JSON.stringify(error);
 			DB.removeJson('twitter');
 			DB.storeJson('twitter',tweets);
 			// connect to a mongo db a dump the return json into it;
 		});
-}
+} */
 
-/* present data */
+/* present data 
 router.get('/result',function(req,res,next){
 	var MongoClient = require('mongodb').MongoClient, assert = require('assert');
 	var url = config.mongo.url;
@@ -80,7 +108,7 @@ router.get('/result',function(req,res,next){
 						});
 				}
 	});
-});
+}); */
 
 
 module.exports = router;
